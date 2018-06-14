87cbf1e863a2e127ecd42f11fc9daf7b0a9c2fd0
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-14 23:38:08 +0530

    successfully done

--- problem4/AreaOfCircle.java
+++ problem4/AreaOfCircle.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-class AreaOfCircle
-{
-public static void main(String args[])
-{
-Scanner s=new Scanner(System.in);
-System.out.println("enter the radius");
-int radius=s.nextInt();
-double aoc=3.14* radius * radius;
-System.out.println("Area of circle is:" + aoc);
-s.close();
-}
+public class AreaOfCircle{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the radius");
+		int radius=s.nextInt();
+		double aoc=3.14 * radius * radius;
+		System.out.println("Area of circle is:" + aoc);
+		s.close();
+		}
 }
