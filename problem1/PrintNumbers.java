a4e94bbb751fc6f52a85f2db18a43cd75786025e
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-14 22:58:20 +0530

    Formmatted the code

--- problem1/PrintNumbers.java
+++ problem1/PrintNumbers.java
@@ -1,6 +1,6 @@
 public class PrintNumbers {
 	public static void main(String args[]) {
-		for(int count=1;count<=100;count++) {
+		for(int count = 1; count <= 100; count++) {
 			System.out.print(count + "\t" );
 		}
 	}
