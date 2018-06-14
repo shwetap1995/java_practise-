d0a7fedddb2b2a593e95d49e24f16e44195b2027
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-14 23:48:56 +0530

    done

---
+++ problem5/SimpleInterestCalc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SimpleInterestCalc{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Principal:");
+		double principal=s.nextDouble();
+		System.out.println("Enter the Rate:");
+		double rate=s.nextDouble();
+		System.out.println("Enter the Time:");
+		double time=s.nextDouble();
+		double si=(principal * rate * time)/100;
+		System.out.println(si);
+		s.close();
+	}
+}
