ad78b9d238d443a817dc00328ed12b4b30df2f95
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-14 22:54:01 +0530

    Formatted the code

--- problem1/PrintNumbers.java
+++ problem1/PrintNumbers.java
@@ -1,10 +1,7 @@
-class PrintNumbers	
-{
-public static void main(String args[])
-{
-for(int count=1;count<=100;count++)
-{
-System.out.print(count + "\t" );
-}
-}
+public class PrintNumbers {
+	public static void main(String args[]) {
+		for(int count=1;count<=100;count++) {
+			System.out.print(count + "\t" );
+		}
+	}
 }
