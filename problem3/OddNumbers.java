c9002548a0d93afd83df955ee02ecfbaa67aba8b
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-14 23:33:38 +0530

    successfully changed

---
+++ problem3/OddNumbers.java
@@ -0,0 +1,9 @@
+public class OddNumbers{
+	public static void main(String args[]){
+		for(int count = 1;count <= 100; count++){
+			if(count%2!=0){
+				System.out.println(count);
+				}
+			}
+		}
+}
