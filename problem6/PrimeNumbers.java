f985a3128064200e840e0aec014f657e78bb46da
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-15 17:47:44 +0530

    logic still not working

--- problem6/PrimeNumbers.java
+++ problem6/PrimeNumbers.java
@@ -3,19 +3,29 @@ public class PrimeNumbers{
 	public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the number:");
-	int num=s.nextInt();	
-		if(num%2!=0){
-			for(int i=2;i<=num;i++){
-	  			if(num%i==0){
-					System.out.println("number is a prime");
-				     		
-				} 
-			}	
-		}
-		else {
-			System.out.println("number is not a prime");
-     		}	
-	s.close();
+	int InputNumber=s.nextInt();	
+		if(InputNumber%2==0){
+                System.out.println("Number is not prime");	
+                break;	
+	       }
+		else if(InputNumber==2){
+			System.out.println("Number is a prime");
+              		break;
+
+       		}
+		else{	
+			for(int i=2;i<InputNumber;i++){
+				if(InputNumber%i==0){
+					System.out.println("Number is not prime");
+              				break;
+				}
+					
+                                {
+                                        System.out.println("number is a prime");
+				}
+			}
+
+s.close();
 	}
 }
 
