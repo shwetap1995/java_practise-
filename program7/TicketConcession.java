3a6e3270168700f718fd5806a059f4910ce3392a
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-19 18:44:45 +0530

    completely done in one attempt yyuuuhhhuuu

---
+++ program7/TicketConcession.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TicketConcession{
+	public static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the age:");
+	int age=s.nextInt();
+	System.out.println("Enter the Distance in KM:");
+	int distance=s.nextInt();
+	double ticketprice=distance * 2.5;
+	if(age < 3){
+		System.out.println("no charges for childrens..happy journey....");
+	}
+	else if(age > 3 && age < 8){
+		System.out.println("Ticket price is:" + ticketprice/2);
+	}
+	else if(age > 60){
+		System.out.println("Ticket price is:" + ticketprice/2);
+	}
+	else{
+		System.out.println("Ticket price is:" + ticketprice);
+	}
+
+	}
+}
