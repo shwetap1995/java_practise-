1107effe5768e0a5e0f9f137ea4a8b85aa1e73b3
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-20 13:07:59 +0530

    code is now in proper format

--- problem4/AreaOfCircle.java
+++ problem4/AreaOfCircle.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class AreaOfCircle {
 
 	public static void main(String args[]) {
-		Scanner s=new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		System.out.println("enter the radius");
-		int radius=s.nextInt();
-		double aoc=3.14 * radius * radius;
+		int radius = s.nextInt();
+		double aoc = 3.14 * radius * radius;
 		System.out.println("Area of circle is:" + aoc);
 		s.close();
 		}
