e7a90af52cbb91cd41d32365dc54f9edf01b970f
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-20 17:24:36 +0530

    using try catch solve the problem

--- problem6/PrimeNumbers.java
+++ problem6/PrimeNumbers.java
@@ -3,29 +3,29 @@
 public class PrimeNumbers {
 
 	public static void main(String args[]) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the number:");
-	int inputNumber = s.nextInt();
-	boolean isPrime = false;
-	if(inputNumber ==  2) {
-		System.out.println("Number is Prime");
-	}	
-	else if(inputNumber % 2 == 0){
-                System.out.println("Number is not prime");
+        	Scanner s=new Scanner(System.in);
+        	System.out.println("Enter the number:");
+		int inputNumber = s.nextInt();
+		boolean isPrime = false;
+		if(inputNumber ==  2) {
+			System.out.println("Number is Prime");
+		}	
+		else if(inputNumber % 2 == 0) {
+                	System.out.println("Number is not prime");
 
-	} else{	
-	    for(int i = 2;i < inputNumber;i++){
-		if(inputNumber % i == 0){
-			isPrime = false;
-              		break;
-		}
-	    if(isPrime) {
-                System.out.println("number is a prime");
-		} else  {
+		} else {	
+	    		for(int i = 2;i < inputNumber;i++) {
+				if(inputNumber % i == 0) {
+					isPrime = false;
+              				break;
+				}
+	    		if(isPrime) {
+                		System.out.println("number is a prime");
+			} else  {
 	
-                System.out.println("number is not prime");
-		}
-	s.close();
+                		System.out.println("number is not prime");
+			  }
+		s.close();
 	}
 	}	
 }
