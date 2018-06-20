f46a9711a6328852b820fadad604fcc7663330cf
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-20 17:36:19 +0530

    The problem has been resolved using the switch case

---
+++ problem8/UsingSwitch.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class UsingSwitch {
+
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("enter the number between 1 to 7:");
+		int number=s.nextInt();
+                switch (number) {
+    			case 1:
+       			System.out.println("Monday");
+        		break;
+
+  		 	case 2:
+        		System.out.println("Tuesday");
+        		break;
+         
+			case 3:
+        		System.out.println("Wednesday");
+        		break;
+			
+			case 4:
+        		System.out.println("Thursday");
+        		break;
+
+			case 5:
+        		System.out.println("Friday");
+        		break;
+			
+			case 6:
+       			System.out.println("Saturday");
+        		break;
+
+			case 7:
+        		System.out.println("Sunday");
+        		break;
+   
+ 			default:
+        		System.out.println("Please enter the number between 1 to 7...!!!");
+		}
+
+	s.close();
+}
+}
