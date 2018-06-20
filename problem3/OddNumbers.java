e70774a1f49f02ce842d261f7e1933fba3c0f538
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-20 12:20:55 +0530

    Removed some old classes

--- problem3/OddNumbers.java
+++ problem3/OddNumbers.java
@@ -1,7 +1,8 @@
-public class OddNumbers{
-	public static void main(String args[]){
-		for(int count = 1;count <= 100; count++){
-			if(count%2!=0){
+public class OddNumbers {
+
+	public static void main(String args[]) {
+		for(int count = 1;count <= 100; count++) {
+			if(count%2!=0) {
 				System.out.println(count);
 				}
 			}
