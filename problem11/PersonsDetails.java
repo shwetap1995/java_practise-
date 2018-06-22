147cd68128d582db85f0836991e27d163c273d00
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-22 11:57:31 +0530

    if statement now  working

--- problem11/PersonsDetails.java
+++ problem11/PersonsDetails.java
@@ -6,15 +6,22 @@ public class PersonsDetails {
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter your name:");
 		String pname = s.next(); 
-		System.out.println("Enter your Gender:");
+		System.out.println("Enter your Gender:(m/f)");
 		String gender = s.next();
-		System.out.println("your marital status:");
+		System.out.println("your marital status:(s/m)");
 		String mstatus = s.next();
-		if( gender == "female" || gender =="F"||gender =="FEMALE"||gender 
-            =="Female"||gender =="f")
-{
-		System.out.println("Enter your Gender:");
-}		
+		if( gender.equals("f") && mstatus.equals("s") ) {
+			System.out.println("Miss" + " " + pname);
+		}	
+		else if( gender.equals("f") && mstatus.equals("m") ) {
+			System.out.println("misses" + " " + pname);
+		}
+		else if( gender.equals("m")) {
+			System.out.println("Mr" + " " + pname);
+		}
+		else {
+			System.out.println("Please enter correct choice");
+		}
 		s.close();
 	}
 }
