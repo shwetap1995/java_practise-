5aef8f0ae12ecc4873412a15ebe8795625c4c5cb
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-22 16:51:53 +0530

    ....

--- problem12/FullName.java
+++ problem12/FullName.java
@@ -1,28 +1,13 @@
-/*
-Take User's full name in following format,
-"firstname middlename lastname" and print it in following format
-First Name:
-Middle Name:
-Surname: 
-*/
-
 import java.util.Scanner;
-
-public class FullName {
-
-	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		String str[] = new String[3];
-		System.out.println("Format : firstname-middlename-lastname");
-		str[0] = s.next();
-		str[1] = s.next();
-		str[2] = s.next();
-        for (String str[]: Str.split(" ", 3)) {
-         System.out.println(str[0]);
-      }
-		
-		s.close();
-		
-	}
+public class FullName{
+   public static void main(String args[]){
+Scanner s = new Scanner(System.in);
+System.out.println("fname mname lname");
+String str = s.next(); 
+       String pstr = new String(str);
+       String array1[]= str.split(" ");
+       for (String temp: array1){
+          System.out.println(temp);
+       }
 }
-
+}
