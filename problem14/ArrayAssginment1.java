d7b58871f2fbd44913c966ecf95768cdbad1bb9c
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-22 17:26:46 +0530

    array related assignment solved

---
+++ problem14/ArrayAssginment1.java
@@ -0,0 +1,12 @@
+public class ArrayAssginment1 {
+	public static void main(String args[]) {
+	int arraySample[]=new int[5];
+    	arraySample[0]=10;
+    	arraySample[1]=20;  
+    	arraySample[2]=70;  
+    	arraySample[3]=40;  
+    	arraySample[4]=50;  
+  	for(int i = 0; i < arraySample.length;i++)
+	System.out.println(arraySample[i]);
+	}
+}
