7fe0c6fc0ce3009851e1da0b49fb2262e8864575
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-06-23 18:08:13 +0530

    runnable but still incomplete

---
+++ problem19/SmallestNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SmallestNumber {
+
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the 5 numbers");
+		int no[] = new int[5];
+		for(int i = 0;i < no.length;i++) {
+			no[i] = s.nextInt();
+		}
+		if(no[0] < no[1] && no[0] < no[2] && no[0] < no[3] && no[0] < no[4]) {
+			System.out.println(no[0] +" " + "is largest");
+		}
+		else if(no[1] < no[0] && no[1] < no[2] && no[1] < no[3] && no[1] < no[4]) {
+			System.out.println(no[1] +" " + "is largest");
+		}
+		else if(no[2] < no[0] && no[2] < no[1] && no[2] < no[3] && no[2] < no[4]) {
+			System.out.println(no[2] +" " + "is largest");
+		}
+		else if(no[3] < no[0] && no[3] < no[1] && no[3] < no[2] && no[3] < no[4]) {
+			System.out.println(no[3] +" " + "is largest");
+		}
+		else if(no[4] < no[0] && no[4] < no[1] && no[4] < no[2] && no[4] < no[3]) {
+			System.out.println(no[4] +" " + "is largest");
+		}
+		
+		s.close();
+	}
+}
