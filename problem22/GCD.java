c17efde91c05b5595045dd4995b17c297011f774
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-07-11 15:36:11 +0530

    its shows gcd and lcm of two numbers

--- problem22/GCD.java
+++ problem22/GCD.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class GCD {
-
+	int n1,n2 = 0; 
 	public static void main(String args[]) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter the two numbers");
 		int n1 = s.nextInt();
 		int n2 = s.nextInt();
+		int number1 = n1;
+		int number2 = n2;
 		while(n1 != n2) {
 			if(n1 > n2) {
 				n1 = n1 - n2;
@@ -17,6 +19,7 @@ public class GCD {
 		}
 	       int gcd = n1;
 	       System.out.println(gcd);	
-		
+		int lcm = number1 * number2 /gcd;
+		System.out.println(lcm);
         }
 }
