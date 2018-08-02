b3c2c75f39a38b3728801b6ac288e4bee74a04aa
patilshwetambari1995@gmail.com <patilshwetambari1995@gmail.com>
2018-08-02 14:31:08 +0530

    Errors have come

---
+++ college_java_assignment /TableOfNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TableOfNo {
+	
+	public static void main(String args[]) {
+		int a;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number");
+		a=Integer.parseInt(args[0]);
+		System.out.println(a);
+		s.close();
+		}
+	}
+
